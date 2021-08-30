10bd5a21b0e7e5e6998adb54cd63dcee4c278880
Nathan Reidy <nathanjreidy@gmail.com>
2021-08-30 21:18:27 +1000

    Added initial routes

--- server.js
+++ server.js
@@ -6,6 +6,32 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+});
+
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+});
+
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Create new bootcamp" });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
