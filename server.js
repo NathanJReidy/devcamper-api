c2471cddfe6e490324e1a1d2d9c48233cbe821e8
Nathan Reidy <nathanjreidy@gmail.com>
2021-09-13 20:06:02 +1000

    Added User Model

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ connectDB();
 // Route files
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
+const auth = require("./routes/auth");
 
 const app = express();
 
@@ -37,6 +38,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // This means we no longer need to specify /api/v1/bootcamps before each of our routes in routes/bootcamps.js
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
 
 app.use(errorHandler);
 
