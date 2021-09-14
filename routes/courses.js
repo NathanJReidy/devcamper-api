05814b908564865151f3b4e789da9245dcb6d992
Nathan Reidy <nathanjreidy@gmail.com>
2021-09-14 23:02:47 +1000

    Auth protect middleware

--- routes/courses.js
+++ routes/courses.js
@@ -12,13 +12,19 @@ const advancedResults = require("../middleware/advancedResults");
 
 const router = express.Router({ mergeParams: true });
 
+const { protect } = require("../middleware/auth");
+
 router
   .route("/")
   .get(
     advancedResults(Course, { path: "bootcamp", select: "name description" }),
     getCourses
   )
-  .post(addCourse);
-router.route("/:id").get(getCourse).put(updateCourse).delete(deleteCourse);
+  .post(protect, addCourse);
+router
+  .route("/:id")
+  .get(getCourse)
+  .put(protect, updateCourse)
+  .delete(protect, deleteCourse);
 
 module.exports = router;
