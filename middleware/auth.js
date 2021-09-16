d526600139f131b37f6e0c56e3dd19000f0c836b
Nathan Reidy <nathanjreidy@gmail.com>
2021-09-17 00:55:53 +1000

    Prevent NoSQL Injection & Sanitize Data

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,12 +11,13 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
+    // Set token from Bearer token in header
     token = req.headers.authorization.split(" ")[1];
+    // Set token from cookie
   }
-
-  //   else if (req.cookies.token) {
-  //     token = req.cookies.token;
-  //   }
+  // else if (req.cookies.token) {
+  //   token = req.cookies.token;
+  // }
 
   // Make sure token exists
   if (!token) {
